47b13fa80ff0b6d688c4f1455a1d919357ab245f
Chris Bridges <chris.bridges@tacit.legal>
2026-02-02 08:22:49 +0000

    fix: gallery wrapping on smaller screens

--- resources/js/components/showcase/show/showcase-gallery.tsx
+++ resources/js/components/showcase/show/showcase-gallery.tsx
@@ -87,7 +87,7 @@ export function ShowcaseGallery({
                 </DialogPortal>
             </Dialog>
             {totalItems > 1 && (
-                <div className="flex gap-2">
+                <div className="flex flex-wrap gap-2">
                     {hasVideo && (
                         <button
                             onClick={() => onSelectIndex(0)}
